9c27ba0dcb019e62deb0cf88cff8deabb629d1fa
GiladFisher <giladfisher98@gmail.com>
2024-02-05 19:04:50 +0200

    postUploadPage should open from amateur view

--- app/src/main/java/com/example/shroomer/HomePageAmateur.kt
+++ app/src/main/java/com/example/shroomer/HomePageAmateur.kt
@@ -1,24 +1,18 @@
 package com.example.shroomer
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-
+import androidx.appcompat.app.AppCompatActivity
+import android.widget.Button
 class HomePageAmateur : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home_page_amateur)
 
-        val newpostButton: Button = findViewById(R.id.bottom_navigation)
+        val newpostButton = findViewById<Button>(R.id.bottom_navigation)
         newpostButton.setOnClickListener {
             val newPostIntent = Intent(this, PostUploadPage::class.java)
             startActivity(newPostIntent)
         }
     }
-//    val loginButton: Button = findViewById(R.id.login_button)
-
-//    signupButton.setOnClickListener {
-//        val signUpIntent = Intent(this, SignUpPage::class.java)
-//        startActivity(signUpIntent)
-//    }
 }
--- app/src/main/java/com/example/shroomer/PostUploadPage.kt
+++ app/src/main/java/com/example/shroomer/PostUploadPage.kt
@@ -1,6 +1,7 @@
 package com.example.shroomer
 
-
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 class PostUploadPage : AppCompatActivity(){
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
